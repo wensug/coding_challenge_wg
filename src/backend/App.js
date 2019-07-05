5b88a3060ae87d15b972216a9f03d8b27e4778fa
wensug <wensug@gmail.com>
2019-07-05 17:14:53 +0100

    backend completed

---
+++ src/backend/App.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+const sortObjectArrayByName = require("./peopleService");
+const port = 3001;
+
+app.get("/rest/people", (req, res) => {
+  res.send({ people: sortObjectArrayByName () });
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
---
+++ src/backend/peopleService.js
@@ -0,0 +1,10 @@
+const db = require('./people.json')
+
+module.exports = function sortObjectArrayByName () {
+    return  db.people.sort((a, b) => {
+        return a.name > b.name ? 1 : -1;
+    });
+}
+
+
+
