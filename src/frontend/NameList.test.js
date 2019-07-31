56cecbe7be57ef78912a8bd0ff45c8b404a02001
wensug <wensug@gmail.com>
2019-07-31 13:21:19 +0100

    testing

---
+++ src/frontend/NameList.test.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import Enzyme, { shallow, configure } from 'enzyme';
+import NameList from './NameList';
+import Adapter from 'enzyme-adapter-react-16';
+configure({ adapter: new Adapter() });
+
+
